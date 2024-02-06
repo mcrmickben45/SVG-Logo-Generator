51bf7095af3a6fcb98d2a8ad24613c5bef843735
Ben McCormick <mr.mccormick@Bens-MacBook-Pro.local>
2024-02-05 17:23:29 -0800

    Troubleshoot deploy, modified index.js

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-const fs = require('fs');
-const { getUserInput } = require('./lib/userInput');
-const { Triangle, Circle, Square } = require('./lib/shapes');
+const inquirer = await import('inquirer');
 
-async function generateLogo() {
-  const userInput = await getUserInput();
+async function getUserInput() {
+  const userInput = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters for the text:',
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter text color (keyword or hexadecimal):',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['circle', 'triangle', 'square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter shape color (keyword or hexadecimal):',
+    },
+  ]);
 
-  let shape;
-  switch (userInput.shape) {
-    case 'circle':
-      shape = new Circle();
-      break;
-    case 'triangle':
-      shape = new Triangle();
-      break;
-    case 'square':
-      shape = new Square();
-      break;
-    default:
-      throw new Error('Invalid shape choice');
-  }
-
-  shape.setColor(userInput.shapeColor);
-
-  const svgContent = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">${shape.render()}</svg>`;
-
-  fs.writeFileSync('logo.svg', svgContent);
-
-  console.log('Generated logo.svg');
+  return userInput;
 }
 
-generateLogo();
+module.exports = { getUserInput };
