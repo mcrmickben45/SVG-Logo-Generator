4da9eb0b475d99acb6f7520f680cd023e6f887d0
Ben McCormick <mr.mccormick@Bens-MacBook-Pro.local>
2024-02-05 17:38:59 -0800

    Change js configuration

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
+const inquirer = require('inquirer');
 const fs = require('fs');
-const { getUserInput } = require('./lib/userInput');
-const { Triangle, Circle, Square } = require('./lib/shapes');
+const questions = require('./lib/userQuestions.js');
+const fileName = "./examples/logo.svg";
+const setShape = require('./lib/chooseShape.js')
 
-async function generateLogo() {
-  const userInput = await getUserInput();
-
-  let shape;
-  switch (userInput.shape) {
-    case 'circle':
-      shape = new Circle();
-      break;
-    case 'triangle':
-      shape = new Triangle();
-      break;
-    case 'square':
-      shape = new Square();
-      break;
-    default:
-      throw new Error('Invalid shape choice');
-  }
-
-  shape.setColor(userInput.shapeColor);
-
-  const svgContent = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">${shape.render()}</svg>`;
-
-  fs.writeFileSync('logo.svg', svgContent);
-
-  console.log('Generated logo.svg');
+function createLogo(response) {
+    const svg = setShape(response);
+    fs.writeFile(fileName, svg, ()=> console.log('Generated logo.svg'));
 }
-
-generateLogo();
+function init() {
+    inquirer 
+    .prompt(questions)
+    .then((response) => {
+        createLogo(response);
+        })
+    .catch(err => {
+            console.log(err)
+        });
+}
+init();
